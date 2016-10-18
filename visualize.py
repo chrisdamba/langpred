c67c312276488a4ae76febf4a47ab089750f6361
chrisdamba <chridam@gmail.com>
2016-10-18 20:29:12 +0100

    IPython Notebook for Exploration

--- visualize.py
+++ visualize.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import bz2
-import os
 import matplotlib.pyplot as plt
+import numpy as np
 import os
+import pickle
 import seaborn as sns
+import sys
 import re
 
 from lang_map import code_lang_map
@@ -14,8 +16,13 @@ from sklearn.metrics.pairwise import pairwise_distances
 from collections import Counter
 from uncurl import parse
 
-
-# regex pattern for scrubbing extracted wikipedia article 
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
 
 """
 Calculate the percentages from a pandas dataframe of letter counts and add the percentages as new columns 
@@ -48,6 +55,7 @@ def get_top_letters():
 
 def parse(filename):
     data = ""
+    # regex pattern for scrubbing extracted wikipedia article 
     article_rgx = re.compile(
     r'<doc id="(?P<id>\d+)" url="(?P<url>[^"]+)" title="(?P<title>[^"]+)">\n(?P<content>.+)\n<\/doc>', re.S | re.U)
     with open(filename, 'r', encoding='utf8') as f:
@@ -60,18 +68,35 @@ def parse(filename):
                     yield m.groupdict()
                 data = ""
 
-def main():    
-    top_letters = get_top_letters()    
-    df = DataFrame(top_letters)
+def load_data():
+    """Load the articles dictionary back from pickle."""
+
+    f = open('letters.pickle', 'rb')
+    data = pickle.load(f, encoding='latin1')
+    f.close()
+    return data
+
+def dump_data():
+    """Save the articles dictionary into pickle."""
+
+    top_letters = get_top_letters()  
+    with open('letters.pickle', 'wb') as handle:
+        pickle.dump(top_letters, handle) 
+
+def main():
+    data = load_data()    
+    df = DataFrame(data)
     df.fillna(0, inplace=True)
-    df = df.set_index('lang')
-
+    df = df.set_index('lang')    
+    
     df3 = percentages(df)
+    df3.values[np.isnan(df3.values)] = np.median(df3.values[~np.isnan(df3.values)])
+    
     num_clusters = 4
-
     palette = sns.color_palette('colorblind', num_clusters)
+    
     # run KMeans clustering algorithm on letter percentages
-    # KMeans automatically groups your data together based on similarity
+    # KMeans automatically groups the data together based on similarity 
     est = KMeans(num_clusters, max_iter=30000)
     est.fit(df3.values)
     y_kmeans = est.predict(df3.values)
@@ -80,13 +105,15 @@ def main():
     pca = PCA(n_components=2)
     pca.fit(df3.values)
     X_trans = pca.transform(df3.values)
+
+    #plot the results
     plt.scatter(X_trans[:, 0], X_trans[:, 1], c=[palette[y] for y in y_kmeans], s=50)
 
-
+    
     cluster_dfs = {}
     cluster_langs = {}
     cluster_distances = {}
-    langs = ['ar', 'bg', 'ca', 'cs', 'da', 'de', 'en']
+    langs = list(code_lang_map.keys())
 
     # Find the languages that are most similar
     for cluster_num in range(4):
@@ -98,9 +125,8 @@ def main():
         # Calculate pairwise distances and display
         print('Cluster #{0}'.format(cluster_num))
 
-        '''
-        Calculate the Euclidian distance between each pair of points - the smaller the distance the more similar the data points are
-        '''
+
+        # Calculate the Euclidian distance between each pair of points - the smaller the distance the more similar the data points are
         cluster_distances[cluster_num] = pairwise_distances(cluster_dfs[cluster_num].values)
         n, m = cluster_distances[cluster_num].shape
         distances = set([])
@@ -115,7 +141,7 @@ def main():
             print(a[1][1])
             print(cluster_langs[cluster_num])
         print()
-
-
+    
+    
 if __name__ == '__main__': 
     main()
